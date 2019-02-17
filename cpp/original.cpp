93292cf04d4348ca4f67bde750116200df9ffa76
mzguimaraes <mzg218@nyu.edu>
2019-02-16 22:24:11 -0500

    Step 2

--- cpp/original.cpp
+++ cpp/original.cpp
@@ -1,25 +1,32 @@
+#define _USE_MATH_DEFINES
+#include <cmath>
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <cstdint>
 #include <cassert>
 
-uint32_t pack_color(const uint8_t r, const uint8_t g, const uint8_t b, const uint8_t a=255) {
-    return (a<<24) + (b<<16) + (g<<8) + r;
+uint32_t pack_color(const uint8_t r, const uint8_t g, const uint8_t b, const uint8_t a = 255)
+{
+    return (a << 24) + (b << 16) + (g << 8) + r;
 }
 
-void unpack_color(const uint32_t &color, uint8_t &r, uint8_t &g, uint8_t &b, uint8_t &a) {
-    r = (color >>  0) & 255;
-    g = (color >>  8) & 255;
+void unpack_color(const uint32_t &color, uint8_t &r, uint8_t &g, uint8_t &b, uint8_t &a)
+{
+    r = (color >> 0) & 255;
+    g = (color >> 8) & 255;
     b = (color >> 16) & 255;
     a = (color >> 24) & 255;
 }
 
-void drop_ppm_image(const std::string filename, const std::vector<uint32_t> &image, const size_t w, const size_t h) {
-    assert(image.size() == w*h);
+void drop_ppm_image(const std::string filename, const std::vector<uint32_t> &image, const size_t w, const size_t h)
+{
+    assert(image.size() == w * h);
     std::ofstream ofs(filename);
-    ofs << "P6\n" << w << " " << h << "\n255\n";
-    for (size_t i = 0; i < h*w; ++i) {
+    ofs << "P6\n"
+        << w << " " << h << "\n255\n";
+    for (size_t i = 0; i < h * w; ++i)
+    {
         uint8_t r, g, b, a;
         unpack_color(image[i], r, g, b, a);
         ofs << static_cast<char>(r) << static_cast<char>(g) << static_cast<char>(b);
@@ -27,17 +34,69 @@ void drop_ppm_image(const std::string filename, const std::vector<uint32_t> &ima
     ofs.close();
 }
 
-int main() {
-    const size_t win_w = 512; // image width
-    const size_t win_h = 512; // image height
-    std::vector<uint32_t> framebuffer(win_w*win_h, 255); // the image itself, initialized to red
+void draw_rectangle(std::vector<uint32_t> &img, const size_t img_w, const size_t img_h, const size_t x, const size_t y, const size_t w, const size_t h, const uint32_t color)
+{
+    assert(img.size() == img_w * img_h);
+    for (size_t i = 0; i < w; i++)
+    {
+        for (size_t j = 0; j < h; j++)
+        {
+            size_t cx = x + i;
+            size_t cy = y + j;
+            assert(cx < img_w && cy < img_h);
+            img[cx + cy * img_w] = color;
+        }
+    }
+}
 
-    for (size_t j = 0; j<win_h; j++) { // fill the screen with color gradients
-        for (size_t i = 0; i<win_w; i++) {
-            uint8_t r = 255*j/float(win_h); // varies between 0 and 255 as j sweeps the vertical
-            uint8_t g = 255*i/float(win_w); // varies between 0 and 255 as i sweeps the horizontal
+int main()
+{
+    const size_t win_w = 512;                              // image width
+    const size_t win_h = 512;                              // image height
+    std::vector<uint32_t> framebuffer(win_w * win_h, 255); // the image itself, initialized to white
+
+    const size_t map_w = 16; // map width
+    const size_t map_h = 16; // map height
+    const char map[] = "0000222222220000"
+                       "1              0"
+                       "1      11111   0"
+                       "1     0        0"
+                       "0     0  1110000"
+                       "0     3        0"
+                       "0   10000      0"
+                       "0   0   11100  0"
+                       "0   0   0      0"
+                       "0   0   1  00000"
+                       "0       1      0"
+                       "2       1      0"
+                       "0       0      0"
+                       "0 0000000      0"
+                       "0              0"
+                       "0002222222200000";    // our game map
+    assert(sizeof(map) == map_w * map_h + 1); // +1 for the null terminated string
+
+    for (size_t j = 0; j < win_h; j++)
+    { // fill the screen with color gradients
+        for (size_t i = 0; i < win_w; i++)
+        {
+            uint8_t r = 255 * j / float(win_h); // varies between 0 and 255 as j sweeps the vertical
+            uint8_t g = 255 * i / float(win_w); // varies between 0 and 255 as i sweeps the horizontal
             uint8_t b = 0;
-            framebuffer[i+j*win_w] = pack_color(r, g, b);
+            framebuffer[i + j * win_w] = pack_color(r, g, b);
+        }
+    }
+
+    const size_t rect_w = win_w / map_w;
+    const size_t rect_h = win_h / map_h;
+    for (size_t j = 0; j < map_h; j++)
+    { // draw the map
+        for (size_t i = 0; i < map_w; i++)
+        {
+            if (map[i + j * map_w] == ' ')
+                continue; // skip empty spaces
+            size_t rect_x = i * rect_w;
+            size_t rect_y = j * rect_h;
+            draw_rectangle(framebuffer, win_w, win_h, rect_x, rect_y, rect_w, rect_h, pack_color(0, 255, 255));
         }
     }
 
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::fs;
 use std::fs::File;
 use std::io::{BufWriter, Write};
+use std::path::Path;
 
 fn pack_color_rgba(r: u8, g: u8, b: u8, a: u8) -> u32 {
     let b1 = r as u32;
@@ -10,6 +11,7 @@ fn pack_color_rgba(r: u8, g: u8, b: u8, a: u8) -> u32 {
     (b4 << 24) + (b3 << 16) + (b2 << 8) + b1
 }
 
+//rust does not have operator overloading
 fn pack_color_rgb(r: u8, g: u8, b: u8) -> u32 {
     pack_color_rgba(r, g, b, 255)
 }
@@ -44,25 +46,101 @@ fn drop_ppm_image(filename: &str, image: &Vec<u32>, w: usize, h: usize) -> std::
 
         output.write(&[r, g, b])?;
     }
-
     //output closes at end of scope
     Ok(())
 }
 
+fn draw_rectangle(
+    img: &mut Vec<u32>,
+    img_w: usize,
+    img_h: usize,
+    x: usize,
+    y: usize,
+    w: usize,
+    h: usize,
+    color: u32,
+) {
+    // assert!(img.len() == img_w * img_h);
+    //move these asserts out of the loop for performance
+    // assert!(x + w < img_w && y + h < img_h);
+
+    for i in 0..w {
+        for j in 0..h {
+            let cx = x + i;
+            let cy = y + j;
+
+            img[cx + cy * img_w] = color;
+        }
+    }
+}
+
 fn main() -> std::io::Result<()> {
     let filename = "./out.ppm";
-    fs::remove_file(filename)?;
+    if Path::new(filename).exists() {
+        fs::remove_file(filename)?;
+    }
 
     let win_w: usize = 512;
     let win_h: usize = 512;
     let mut framebuffer: Vec<u32> = vec![255; win_w * win_h];
 
+    let map_w: usize = 16;
+    let map_h: usize = 16;
+    let mut map: Vec<&str> = "0000222222220000\
+                              1              0\
+                              1      11111   0\
+                              1     0        0\
+                              0     0  1110000\
+                              0     3        0\
+                              0   10000      0\
+                              0   0   11100  0\
+                              0   0   0      0\
+                              0   0   1  00000\
+                              0       1      0\
+                              2       1      0\
+                              0       0      0\
+                              0 0000000      0\
+                              0              0\
+                              0002222222200000"
+        .split("")
+        .collect(); // our game map
+                    //strip out null chars
+    map.remove(map_w * map_h - 1);
+    map.remove(0);
+
+    // println!("{:?}", map);
+    assert_eq!(map.len(), map_w * map_h);
+
     for j in 0..win_h {
+        //fill screen with a color gradient
         for i in 0..win_w {
             let r: u8 = (255.0 * (j as f32) / (win_h as f32)) as u8;
             let g: u8 = (255.0 * (i as f32) / (win_w as f32)) as u8;
             let b: u8 = 0;
             framebuffer[i + j * win_w] = pack_color_rgb(r, g, b);
+        }
+    }
+
+    let rect_w = win_w / map_w;
+    let rect_h = win_h / map_h;
+    for j in 0..map_h {
+        //draw the map
+        for i in 0..map_w {
+            if map[i + j * map_w] == " " {
+                continue;
+            } //skip empty spaces
+            let rect_x = i * rect_w;
+            let rect_y = j * rect_h;
+            draw_rectangle(
+                &mut framebuffer,
+                win_w,
+                win_h,
+                rect_x,
+                rect_y,
+                rect_w,
+                rect_h,
+                pack_color_rgb(0, 255, 255),
+            );
         }
     }
 
